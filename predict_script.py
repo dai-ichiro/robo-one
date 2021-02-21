445d8a07631fd3f1c464b10508467e58939008f2
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-02-21 17:37:01 +0900

    script for prediction

---
+++ predict_script.py
@@ -0,0 +1,30 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from mxnet import image
+from gluoncv import utils
+
+import autogluon.core as ag
+from autogluon.vision import ObjectDetector
+
+detector = ObjectDetector.load('enemy_detector.ag')
+
+img_file = 'IMG_2293.jpg'
+
+image_array = image.imread(img_file)
+
+result = detector.predict(image_array)
+selected_result = result[result['predict_score']>0.8]
+
+class_names = list(set(selected_result['predict_class']))
+
+bounding_boxes = np.array([[x[i] for i in x.keys()] for x in list(selected_result.iloc[:,2])])
+
+scores = np.array(selected_result.iloc[:,1])
+
+class_ids = np.array([class_names.index(i) for i in list(selected_result.iloc[:,0])])
+
+utils.viz.plot_bbox(image_array, bounding_boxes, scores=scores,
+                    labels=class_ids, class_names = class_names, absolute_coordinates=False)
+
+plt.show()
