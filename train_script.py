5f84d02816cd8d28da402d18657ca8dde5423aae
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-02-21 17:29:59 +0900

    script for training

---
+++ train_script.py
@@ -0,0 +1,10 @@
+from autogluon.vision import ObjectDetector
+
+dataset_train = ObjectDetector.Dataset.from_voc('train_data', splits='train')
+
+time_limit = 5*60*60  # 5 hour
+detector = ObjectDetector()
+hyperparameters = {'batch_size':4, 'epochs': 2}
+detector.fit(dataset_train, time_limit=time_limit, num_trials=2, hyperparameters=hyperparameters)
+
+detector.save('enemy_detector.ag')
