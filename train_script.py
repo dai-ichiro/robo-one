1c9da904d1f2d901adc5fed1b09e594842114ff6
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2021-08-15 22:59:35 +0900

    Update train_script.py

--- train_script.py
+++ train_script.py
@@ -5,6 +5,6 @@ dataset_train = ObjectDetector.Dataset.from_voc('train_data', splits='train')
 time_limit = 5*60*60  # 5 hour
 detector = ObjectDetector()
 hyperparameters = {'batch_size':4, 'epochs': 2}
-detector.fit(dataset_train, time_limit=time_limit, num_trials=2, hyperparameters=hyperparameters)
+detector.fit(dataset_train, time_limit=time_limit, hyperparameters=hyperparameters)
 
 detector.save('enemy_detector.ag')
