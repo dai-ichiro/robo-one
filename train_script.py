794a1cd68dbbe0ea715b8a1abb25b68a8f8f91f4
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-12-24 14:04:12 +0900

    modify train_script.py

--- train_script.py
+++ train_script.py
@@ -1,10 +1,12 @@
 from autogluon.vision import ObjectDetector
+import pandas as pd
 
-dataset_train = ObjectDetector.Dataset.from_voc('train_data', splits='train')
+df = pd.read_pickle('dataset.pkl')
+dataset = ObjectDetector.Dataset(df, classes=df['class'].unique().tolist())
 
 time_limit = 5*60*60  # 5 hour
 detector = ObjectDetector()
 hyperparameters = {'batch_size':4, 'epochs': 2}
-detector.fit(dataset_train, time_limit=time_limit, hyperparameters=hyperparameters)
+detector.fit(dataset, time_limit=time_limit, hyperparameters=hyperparameters)
 
 detector.save('enemy_detector.ag')
